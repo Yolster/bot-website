d747aad661ca92efd58935993da27a1a74e01964
Yolster <yolster@protonmail.com>
2021-08-26 14:01:06 +0300

    Update codes

--- bots/cmds/ping.js
+++ bots/cmds/ping.js
@@ -1,7 +1,23 @@
 const Discord = require('discord.js')
+const www = require('../../bot')
 
 exports.run = async(bot, message) => {
-  message.channel.send(`${bot.ws.ping}`)
+  var guildid = message.guild.id;
+  const veri = await new Promise((resolve, reject) => {
+    www.connection.query(`SELECT * FROM guilds WHERE guildid = ?`, [guildid], function (err, result) {
+        if (err)
+            reject(err);
+        resolve(result);
+    });
+});
+
+  if(veri[0].lang === "tr"){
+  message.channel.send(`Pingim : ${bot.ws.ping}`)
+}
+
+if(veri[0].lang === "en"){
+  message.channel.send(`My ping : ${bot.ws.ping}`)
+}
 }
 exports.conf = {
   enabled: true,
